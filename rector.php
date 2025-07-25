4c236f0a63a7ac7451004250cb7bf91e20cd6315
Petr Král <kral.petr.88@gmail.com>
2025-07-25 14:39:50 +0200

    PHPCS - fixes

--- rector.php
+++ rector.php
@@ -2,12 +2,11 @@
 
 declare(strict_types = 1);
 
-
 use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
 
-    foreach (include(__DIR__.'/rules/rules.php') as $ruleClassName) {
+    foreach (include(__DIR__ . '/rules/rules.php') as $ruleClassName) {
         $rectorConfig->rule($ruleClassName);
     }
 };
