facec70662771e826bf949b139f4b5784735d927
Petr Král <kral.petr.88@gmail.com>
2025-09-08 17:18:20 +0200

    chore(composer) - create composer scripts and modify github actions

--- rector.php
+++ rector.php
@@ -5,7 +5,6 @@ declare(strict_types = 1);
 use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
-
     foreach (include(__DIR__ . '/rules/rules.php') as $ruleClassName) {
         $rectorConfig->rule($ruleClassName);
     }
